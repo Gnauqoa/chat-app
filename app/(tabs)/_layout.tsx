848c6fa5f6bd8c7e06beac0427fc10fe1310c37d
Havold <21521479@gm.uit.edu.vn>
2024-01-17 18:09:21 +0700

    Login Screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,76 +1,76 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
-import { Pressable, TouchableOpacity, useColorScheme } from "react-native";
-import Colors from "../../constants/Colors";
-import { Entypo, MaterialIcons } from "@expo/vector-icons";
-import useScreenOrientation from "../../hooks/useScreenOrientation";
+// import FontAwesome from "@expo/vector-icons/FontAwesome";
+// import { Link, Tabs } from "expo-router";
+// import { Pressable, TouchableOpacity, useColorScheme } from "react-native";
+// import Colors from "../../constants/Colors";
+// import { Entypo, MaterialIcons } from "@expo/vector-icons";
+// import useScreenOrientation from "../../hooks/useScreenOrientation";
 
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
-}) {
-  return <FontAwesome size={20} style={{ marginBottom: -3 }} {...props} />;
-}
+// /**
+//  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+//  */
+// function TabBarIcon(props: {
+//   name: React.ComponentProps<typeof FontAwesome>["name"];
+//   color: string;
+// }) {
+//   return <FontAwesome size={20} style={{ marginBottom: -3 }} {...props} />;
+// }
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  const { handleToggle } = useScreenOrientation();
-  return (
-    <Tabs
-      screenOptions={{
-        headerLeft: () => (
-          <TouchableOpacity onPress={handleToggle}>
-            <MaterialIcons
-              style={{ marginLeft: 15 }}
-              name="screen-rotation"
-              size={25}
-              color={Colors[colorScheme ?? "light"].text}
-            />
-          </TouchableOpacity>
-        ),
-        headerRight: () => (
-          <Link href="/History" asChild>
-            <Pressable>
-              {({ pressed }) => (
-                <MaterialIcons
-                  name="history"
-                  size={25}
-                  color={Colors[colorScheme ?? "light"].text}
-                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                />
-              )}
-            </Pressable>
-          </Link>
-        ),
-        tabBarActiveTintColor: "#f09a36",
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Converter",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name="code" color={focused ? "#f09a36" : color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="calculator"
-        options={{
-          title: "Calculator",
-          tabBarIcon: ({ color, focused }) => (
-            <Entypo
-              size={20}
-              name="calculator"
-              color={focused ? "#f09a36" : color}
-            />
-          ),
-          tabBarActiveTintColor: "#f09a36",
-        }}
-      />
-    </Tabs>
-  );
-}
+// export default function TabLayout() {
+//   const colorScheme = useColorScheme();
+//   const { handleToggle } = useScreenOrientation();
+//   return (
+//     <Tabs
+//       screenOptions={{
+//         headerLeft: () => (
+//           <TouchableOpacity onPress={handleToggle}>
+//             <MaterialIcons
+//               style={{ marginLeft: 15 }}
+//               name="screen-rotation"
+//               size={25}
+//               color={Colors[colorScheme ?? "light"].text}
+//             />
+//           </TouchableOpacity>
+//         ),
+//         headerRight: () => (
+//           <Link href="/History" asChild>
+//             <Pressable>
+//               {({ pressed }) => (
+//                 <MaterialIcons
+//                   name="history"
+//                   size={25}
+//                   color={Colors[colorScheme ?? "light"].text}
+//                   style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+//                 />
+//               )}
+//             </Pressable>
+//           </Link>
+//         ),
+//         tabBarActiveTintColor: "#f09a36",
+//       }}
+//     >
+//       <Tabs.Screen
+//         name="index"
+//         options={{
+//           title: "Converter",
+//           tabBarIcon: ({ color, focused }) => (
+//             <TabBarIcon name="code" color={focused ? "#f09a36" : color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="calculator"
+//         options={{
+//           title: "Calculator",
+//           tabBarIcon: ({ color, focused }) => (
+//             <Entypo
+//               size={20}
+//               name="calculator"
+//               color={focused ? "#f09a36" : color}
+//             />
+//           ),
+//           tabBarActiveTintColor: "#f09a36",
+//         }}
+//       />
+//     </Tabs>
+//   );
+// }
--- section/calculator/landscape.tsx
+++ section/calculator/landscape.tsx
@@ -1,292 +1,173 @@
-import { AntDesign, Feather } from "@expo/vector-icons";
-import { SafeAreaView, Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import useCalculation, { Operator } from "../../hooks/useCalculation";
-import Button, { ButtonProps } from "../../components/Button";
-import Row from "../../components/Row";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity,StyleSheet, Image, Alert, Keyboard } from 'react-native';
+import color from './../../container/color'
 
-const Landscape = () => {
-  const {
-    display,
-    handleDelete,
-    handleCopy,
-    handlePaste,
-    handleNumberPress,
-    handleDecimalPress,
-    handleOperatorPress,
-    handleEqualsPress,
-    handleClearPress,
-    handleTrigPress,
-    handleLogPress,
-    handleLnPress,
-    handleInversePress,
-    handleExponentialPress,
-    handleSquarePress,
-    handlePowerPress,
-    handleAbsolutePress,
-    handlePiPress,
-    handleEPress,
-    handleRadPress,
-    handleSquareRootPress,
-    handleCubeRootPress,
-    operator,
-  } = useCalculation();
-  const render: ButtonProps[][] = [
-    [
-      {
-        operator: operator,
-        value: "rad",
-        onPress: handleRadPress,
-      },
-      {
-        operator: operator,
-        value: "∛",
-        onPress: handleCubeRootPress,
-      },
-      {
-        operator: operator,
-        value: "√",
-        onPress: handleSquareRootPress,
-      },
-      {
-        operator: operator,
-        value: "C",
-        style: "secondary",
-        onPress: handleClearPress,
-      },
-      {
-        operator: operator,
-        value: Operator.ToggleSign,
-        style: "secondary",
-        onPress: () => handleOperatorPress(Operator.ToggleSign),
-      },
-      {
-        operator: operator,
-        value: Operator.Division100,
-        style: "secondary",
-        onPress: () => handleOperatorPress(Operator.Division100),
-      },
-      {
-        operator: operator,
-        value: Operator.Division,
-        style: "accent",
-        onPress: () => handleOperatorPress(Operator.Division),
-      },
-    ],
-    [
-      {
-        operator: operator,
-        value: "sin",
-        onPress: () => handleTrigPress(Operator.Sin),
-      },
-      {
-        operator: operator,
-        value: "cos",
-        onPress: () => handleTrigPress(Operator.Cos),
-      },
-      {
-        operator: operator,
-        value: "tan",
-        onPress: () => handleTrigPress(Operator.Tan),
-      },
-      {
-        operator: operator,
-        value: "7",
-        onPress: () => handleNumberPress("7"),
-      },
-      {
-        operator: operator,
-        value: "8",
-        onPress: () => handleNumberPress("8"),
-      },
-      {
-        operator: operator,
-        value: "9",
-        onPress: () => handleNumberPress("9"),
-      },
-      {
-        operator: operator,
-        value: Operator.Multiplication,
-        style: "accent",
-        onPress: () => handleOperatorPress(Operator.Multiplication),
-      },
-    ],
-    [
-      {
-        operator: operator,
-        value: "ln",
-        onPress: handleLnPress,
-      },
-      {
-        operator: operator,
-        value: "log",
-        onPress: handleLogPress,
-      },
-      {
-        operator: operator,
-        value: "1/x",
-        onPress: handleInversePress,
-      },
-      {
-        operator: operator,
-        value: "4",
-        onPress: () => handleNumberPress("4"),
-      },
-      {
-        operator: operator,
-        value: "5",
-        onPress: () => handleNumberPress("5"),
-      },
-      {
-        operator: operator,
-        value: "6",
-        onPress: () => handleNumberPress("6"),
-      },
-      {
-        operator: operator,
-        value: Operator.Subtraction,
-        style: "accent",
-        onPress: () => handleOperatorPress(Operator.Subtraction),
-      },
-    ],
-    [
-      {
-        operator: operator,
-        value: "e^x",
-        onPress: handleExponentialPress,
-      },
-      {
-        operator: operator,
-        value: "x^2",
-        onPress: handleSquarePress,
-      },
-      {
-        operator: operator,
-        value: "x^y",
-        onPress: handlePowerPress,
-      },
-      {
-        operator: operator,
-        value: "1",
-        onPress: () => handleNumberPress("1"),
-      },
-      {
-        operator: operator,
-        value: "2",
-        onPress: () => handleNumberPress("2"),
-      },
-      {
-        operator: operator,
-        value: "3",
-        onPress: () => handleNumberPress("3"),
-      },
-      {
-        operator: operator,
-        value: Operator.Addition,
-        style: "accent",
-        onPress: () => handleOperatorPress(Operator.Addition),
-      },
-    ],
-    [
-      {
-        operator: operator,
-        value: "|x|",
-        onPress: handleAbsolutePress,
-      },
-      {
-        operator: operator,
-        value: "π",
-        onPress: handlePiPress,
-      },
-      {
-        operator: operator,
-        value: "e",
-        onPress: handleEPress,
-      },
-      {
-        operator: operator,
-        value: "0",
-        onPress: () => handleNumberPress("0"),
-      },
-      {
-        operator: operator,
-        value: ".",
-        onPress: handleDecimalPress,
-      },
-      {
-        operator: operator,
-        value: "Paste",
-        style: "accent",
-        onPress: handlePaste,
-      },
-      {
-        operator: operator,
-        value: "=",
-        style: "accent",
-        onPress: handleEqualsPress,
-      },
-    ],
-  ];
+interface LoginProps {
+  // Add any props if needed
+}
+
+const Login: React.FC<LoginProps> = (props) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPwd] = useState<string>("");
+
+  const handleLogIn = (email: string, password: string) => {
+    if (email.length === 0) {
+      Alert.alert("Error", "You have not entered your email yet. Please fill it in");
+      return false;
+    }
+    if (password.length === 0) {
+      Alert.alert("Error", "You have not entered your email yet. Please fill it in");
+      return false;
+    }
+    Alert.alert("Success", "Login successfully");
+    setPwd("");
+    Keyboard.dismiss();
+  }
+
+  const handleForgotPwd = () => {
+    Alert.alert("Forgot Password");
+  }
 
   return (
-    <SafeAreaView style={{ height: "100%", width: "100%" }}>
-      <View
-        style={{
-          flex: 1,
-          paddingVertical: 20,
-          flexDirection: "column",
-          backgroundColor: "#202020",
-          gap: 4,
-        }}
-      >
-        <View
-          style={{
-            paddingBottom: 20,
-            paddingRight: 20,
-            position: "relative",
-            width: "100%",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 16,
-          }}
-        >
-          <Text
-            style={{
-              color: "#fff",
-              fontSize: 40,
-              textAlign: "right",
-              marginLeft: "auto",
-            }}
-          >
-            {display}
+    <View style={styles.container}>
+      <View style={styles.loginContainer}>
+        {/* <Image source={} /> */}
+        <Text style={styles.title}>
+          Log in to Chatbox
+        </Text>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.text}>
+            Your email
           </Text>
-          <TouchableOpacity onPress={handleDelete}>
-            <Feather name="delete" size={24} color="white" />
-          </TouchableOpacity>
-          <View
-            style={{
-              zIndex: 20,
-              position: "absolute",
-              left: 10,
-              top: 0,
-            }}
-          >
-            <TouchableOpacity onPress={handleCopy}>
-              <AntDesign name="copy1" size={18} color="white" />
-            </TouchableOpacity>
+          <View>
+            <TextInput
+              onChangeText={(email) => setEmail(email)}
+              style={styles.input}
+            />
           </View>
         </View>
-        {render.map((row, index) => (
-          <Row key={index + "row"}>
-            {row.map((button, b_index) => (
-              <Button key={index + b_index + "button"} {...button} />
-            ))}
-          </Row>
-        ))}
-      </View>
-    </SafeAreaView>
-  );
-};
 
-export default Landscape;
+        <View style={styles.inputContainer}>
+          <Text style={styles.text}>
+            Password
+          </Text>
+          <View>
+            <TextInput
+              value={password}
+              onChangeText={(password) => setPwd(password)}
+              style={styles.input}
+            />
+          </View>
+        </View>
+
+        {/* Button */}
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => handleLogIn(email, password)}
+        >
+          <View style={styles.button}>
+            <Text style={styles.textBtn}>
+              Log in
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <View style={styles.textBox}>
+          <Text style={styles.smallText}>
+            Welcome back! Sign in using your social account or email to continue us
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={handleForgotPwd}
+        >
+          <Text style={[styles.text, styles.marginTop40]}>
+            Forgot password?
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles =StyleSheet.create({
+  container: {
+    backgroundColor: color.white,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  loginContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    marginTop: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: color.heading,
+  },
+  
+  inputContainer: {
+    marginTop: 20
+  },
+
+  text: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: color.heading,
+  },
+
+  input: {
+    marginTop: 10,
+    width: 350,
+    // backgroundColor: 'red',
+    height: 44,
+    borderRadius: 10,
+    borderColor: color.heading,
+    borderWidth: 1,
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    color: color.black,
+  },
+  
+  button: {
+    width: 350,
+    height: 48,
+    backgroundColor: color.heading,
+    borderRadius: 14,
+    marginTop: 38,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  textBtn: {
+    fontSize: 16,
+    color: color.white,
+    fontWeight: 'bold',
+  },
+
+  textBox: {
+    marginTop: 30,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  smallText: {
+    fontSize: 14,
+    color: color.note,
+    fontWeight: '300',
+    textAlign: 'center',
+  },
+
+  marginTop40: {
+    marginTop: 40,
+  }
+})
+
+export default Login;
